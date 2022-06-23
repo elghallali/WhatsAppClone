e4d2b9eb47fc19adb9430fa9891c67007517b05a
Yassine EL GHALLALI <35077623+elghallali@users.noreply.github.com>
2022-06-23 20:18:14 +0100

    update navigation/index.tsx

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -4,21 +4,21 @@
  *
  */
  import { FontAwesome, Octicons, MaterialCommunityIcons, Fontisto } from '@expo/vector-icons';
- import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
- import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
- import { createNativeStackNavigator } from '@react-navigation/native-stack';
- import * as React from 'react';
- import { ColorSchemeName, Pressable, View } from 'react-native';
- import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import { ColorSchemeName, Pressable, View } from 'react-native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
- import Colors from '../constants/Colors';
- import useColorScheme from '../hooks/useColorScheme';
- import ModalScreen from '../screens/ModalScreen';
- import NotFoundScreen from '../screens/NotFoundScreen';
- import TabOneScreen from '../screens/TabOneScreen';
- import ChatsScreen from '../screens/ChatsScreen';
- import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
- import LinkingConfiguration from './LinkingConfiguration';
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import ModalScreen from '../screens/ModalScreen';
+import NotFoundScreen from '../screens/NotFoundScreen';
+import TabOneScreen from '../screens/TabOneScreen';
+import ChatsScreen from '../screens/ChatsScreen';
+import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
+import LinkingConfiguration from './LinkingConfiguration';
 const Navigation = ({ colorScheme }: { colorScheme: ColorSchemeName }) => {
   return (
     <NavigationContainer
